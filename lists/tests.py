8c9181355c8420faca1e46143272a9f441a8a41e
Csaba Benko <csaba.benko@infoteach.hu>
2015-12-03 04:59:50 +0000

    Basic view now returns minimal html

--- lists/tests.py
+++ lists/tests.py
@@ -1,12 +1,19 @@
 from django.core.urlresolvers import resolve
 from django.test import TestCase
 from lists.views import home_page
+from django.http import HttpRequest
 import inspect
 
 class HomePageTest(TestCase):
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve("/")
-        print(found.func)
-        self.assertEqual(hasattr(found.func, "__call__"), True) # one way to test whether this is a function
-        self.assertEqual(inspect.isfunction(found.func), True)  # another way to test whether this is a function
+        self.assertTrue(hasattr(found.func, "__call__")) # one way to test whether this is a function
+        self.assertTrue(inspect.isfunction(found.func))  # another way to test whether this is a function
         self.assertEqual(found.func.__name__, "home_page")
+
+    def test_home_page_returns_correct_url(self):
+        request = HttpRequest()
+        response = home_page(request)
+        self.assertTrue(response.content.startswith(b"<html>"))
+        self.assertIn(b"<title>To-Do list</title>", response.content)
+        self.assertTrue(response.content.endswith(b"</html>"))
