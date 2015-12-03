24a9b49ab036c8d31177940578aa24224eb91cea
Csaba Benko <csaba.benko@infoteach.hu>
2015-12-03 04:42:34 +0000

    First unit test and url mapping, dummy view

--- lists/tests.py
+++ lists/tests.py
@@ -1,5 +1,12 @@
+from django.core.urlresolvers import resolve
 from django.test import TestCase
+from lists.views import home_page
+import inspect
 
-class SmokeTest(TestCase):
-    def test_mad_maths(self):
-        self.assertEqual(1+1, 3)
+class HomePageTest(TestCase):
+    def test_root_url_resolves_to_home_page_view(self):
+        found = resolve("/")
+        print(found.func)
+        self.assertEqual(hasattr(found.func, "__call__"), True) # one way to test whether this is a function
+        self.assertEqual(inspect.isfunction(found.func), True)  # another way to test whether this is a function
+        self.assertEqual(found.func.__name__, "home_page")
