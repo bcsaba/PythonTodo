ebf1df46544435448b5b33509740f40de55e9a79
Csaba Benko <csaba.benko@infoteach.hu>
2015-12-05 05:15:11 +0000

    Refactor home page view to use a template - step 2
    Refactor unit test not to test constants, but the template instead.

--- lists/tests.py
+++ lists/tests.py
@@ -1,7 +1,8 @@
 from django.core.urlresolvers import resolve
 from django.test import TestCase
-from lists.views import home_page
 from django.http import HttpRequest
+from django.template.loader import render_to_string
+from lists.views import home_page
 import inspect
 
 class HomePageTest(TestCase):
@@ -14,6 +15,5 @@ class HomePageTest(TestCase):
     def test_home_page_returns_correct_url(self):
         request = HttpRequest()
         response = home_page(request)
-        self.assertTrue(response.content.startswith(b"<!DOCTYPE HTML PUBLIC"))
-        self.assertIn(b"<title>To-Do list</title>", response.content)
-        self.assertTrue(response.content.strip().endswith(b"</html>"))
+        expected_html = render_to_string("home.html")
+        self.assertEqual(response.content.decoce(), expected_html)
