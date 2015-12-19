1a95b7d184c64131009f57d8553f6246fa335850
Csaba Benko <csaba.benko@infoteach.hu>
2015-12-19 22:40:05 +0000

    Model for list items and associated migration

--- lists/tests.py
+++ lists/tests.py
@@ -3,6 +3,7 @@ from django.test import TestCase
 from django.http import HttpRequest
 from django.template.loader import render_to_string
 from lists.views import home_page
+from lists.models import Item
 import inspect
 
 class HomePageTest(TestCase):
@@ -38,21 +39,23 @@ class HomePageTest(TestCase):
         self.assertEqual(generated_content_no_csrfinput, expected_html)
 
 
-# class ItemModelTest(TestCase):
-#     def test_saving_and_retrieving_items(self):
-#         first_item = Item()
-#         first_item.text = "The first (ever) list item"
-#         first_item.save()
         
-#         second_item = Item()
-#         second_item.text = "Item the second"
-#         second_item.save()
 
-#         saved_items = Item.objects.all()
-#         self.assertEqual(saved_items.count(), 2)
+class ItemModelTest(TestCase):
+    def test_saving_and_retrieving_items(self):
+        first_item = Item()
+        first_item.text = "The first (ever) list item"
+        first_item.save()
+        
+        second_item = Item()
+        second_item.text = "Item the second"
+        second_item.save()
 
-#         first_saved_item = saved_items[0]
-#         second_saved_item = saved_items[1]
-#         self.assertEqual(first_saved_item.text, "The first (ever) list item")
-#         self.assertEqual(second_saved_item.text, "Item the second")
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text, "The first (ever) list item")
+        self.assertEqual(second_saved_item.text, "Item the second")
         
