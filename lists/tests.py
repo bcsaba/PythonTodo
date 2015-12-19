de81223ba762fb9b81297ca12efedad3dedc096e
Csaba Benko <csaba.benko@infoteach.hu>
2015-12-19 04:57:29 +0000

    Refactored unit tests to pass with CSRF hidden filed check also.

--- lists/tests.py
+++ lists/tests.py
@@ -16,7 +16,9 @@ class HomePageTest(TestCase):
         request = HttpRequest()
         response = home_page(request)
         expected_html = render_to_string("home.html")
-        self.assertEqual(response.content.decode(), expected_html)
+        generated_content = response.content.decode()
+        generated_content_no_csrfinput = '\n'.join([line if not "csrfmiddlew" in line else "\t  " for line in generated_content.split('\n')])
+        self.assertEqual(generated_content_no_csrfinput, expected_html)
 
     def test_home_page_can_save_a_post_request(self):
         request = HttpRequest()
@@ -26,8 +28,31 @@ class HomePageTest(TestCase):
         response = home_page(request)
 
         self.assertIn("A new list item", response.content.decode())
+        
         expected_html = render_to_string(
             "home.html",
             {"new_item_text": "A new list item"}
         )
-        self.assertEqual(response.content.decode(), expected_html)
+        generated_content = response.content.decode()
+        generated_content_no_csrfinput = '\n'.join([line if not "csrfmiddlew" in line else "\t  " for line in generated_content.split('\n')])
+        self.assertEqual(generated_content_no_csrfinput, expected_html)
+
+
+# class ItemModelTest(TestCase):
+#     def test_saving_and_retrieving_items(self):
+#         first_item = Item()
+#         first_item.text = "The first (ever) list item"
+#         first_item.save()
+        
+#         second_item = Item()
+#         second_item.text = "Item the second"
+#         second_item.save()
+
+#         saved_items = Item.objects.all()
+#         self.assertEqual(saved_items.count(), 2)
+
+#         first_saved_item = saved_items[0]
+#         second_saved_item = saved_items[1]
+#         self.assertEqual(first_saved_item.text, "The first (ever) list item")
+#         self.assertEqual(second_saved_item.text, "Item the second")
+        
--- superlists/urls.py
+++ superlists/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+import lists.views
 
 urlpatterns = [
-    url(r"^$", "lists.views.home_page", name = "home")
+    url(r"^$", lists.views.home_page, name = "home"),
     #url(r'^admin/', include(admin.site.urls)),
 ]
